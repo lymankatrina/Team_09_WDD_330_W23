d9f2339ab75bd42606ced8cb4c610e8641ac96ee
Katrina Lyman <lymankatrina@gmail.com>
2023-03-02 21:18:55 -0700

    finish step 6

--- src/js/product.js
+++ src/js/product.js
@@ -1,8 +1,11 @@
-import { setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
-import { getParams } from "./utils.mjs";
+import ProductDetails from "./ProductDetails.mjs";
+import { setLocalStorage, getParam } from "./utils.mjs";
 
+const productId = getParam("product");
 const dataSource = new ProductData("tents");
+const product = new ProductDetails(productId, dataSource);
+product.init();
 
 function addProductToCart(product) {
   setLocalStorage("so-cart", product);
