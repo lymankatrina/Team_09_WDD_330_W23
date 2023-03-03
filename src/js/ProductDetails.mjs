d80dc2a0d41c9c4d2c672f40326886e0e3f90712
Katrina Lyman <lymankatrina@gmail.com>
2023-03-02 21:41:03 -0700

    Compared to Instructor

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -1,3 +1,23 @@
+import { setLocalStorage } from "./utils.mjs";
+
+function productDetailsTemplate(product) {
+  return `<section class="product-detail"> <h3>${product.Brand.Name}</h3>
+    <h2 class="divider">${product.NameWithoutBrand}</h2>
+    <img
+      class="divider"
+      src="${product.Image}"
+      alt="${product.NameWithoutBrand}"
+    />
+    <p class="product-card__price">$${product.FinalPrice}</p>
+    <p class="product__color">${product.Colors[0].ColorName}</p>
+    <p class="product__description">
+    ${product.DescriptionHtmlSimple}
+    </p>
+    <div class="product-detail__add">
+      <button id="addToCart" data-id="${product.Id}">Add to Cart</button>
+    </div></section>`;
+}
+
 function convertToJson(res) {
   if (res.ok) {
     return res.json();
@@ -12,13 +32,37 @@ export default class ProductDetails {
     this.product = {};
     this.dataSource = dataSource;
   }
-  getData() {
-    return fetch(this.path)
-      .then(convertToJson)
-      .then((data) => data);
+
+  async init() {
+    // use our datasource to get the details for the current product. findProductById will return a promise! use await or .then() to process it
+    this.product = await this.dataSource.findProductById(this.productId);
+    // once we have the product details we can render out the HTML
+    this.renderProductDetails("main");
+    // once the HTML is rendered we can add a listener to Add to Cart button
+    // Notice the .bind(this). Our callback will not work if we don't include that line. Review the readings from this week on 'this' to understand why.
+    document
+      .getElementById("addToCart")
+      .addEventListener("click", this.addToCart.bind(this));
   }
-  async findProductById(id) {
-    const products = await this.getData();
-    return products.find((item) => item.Id === id);
+  addToCart() {
+    setLocalStorage("so-cart", this.product);
+  }
+  renderProductDetails(selector) {
+    const element = document.querySelector(selector);
+    element.insertAdjacentHTML(
+      "afterBegin",
+      productDetailsTemplate(this.product)
+    );
   }
 }
+
+//   getData() {
+//     return fetch(this.path)
+//       .then(convertToJson)
+//       .then((data) => data);
+//   }
+//   async findProductById(id) {
+//     const products = await this.getData();
+//     return products.find((item) => item.Id === id);
+//   }
+// }
