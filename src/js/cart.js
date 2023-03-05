41c683e9f413e9ec1ce89fd99586ed2429df5e74
Teresa Moser <moserte@byui.edu>
2023-03-04 23:30:02 -0700

    Update Backpack number

--- src/js/cart.js
+++ src/js/cart.js
@@ -24,5 +24,33 @@ function cartItemTemplate(item) {
 
   return newItem;
 }
-
 renderCartContents();
+
+let carts = document.querySelectorAll(".add-cart");
+
+for (let i=0; i < carts.length; i++ ) {
+  carts[i].addEventListener("click", () => {
+    cartNumbers(products[i]);
+  })
+
+}
+function onLoadCartNumbers(){
+  let productNumbers = localStorage.getItem("cartNumbers");
+  if (productNumbers){
+    document.querySelector(".cart span").textContent = productNumbers;
+  }
+}
+function cartNumbers() {
+  let productNumbers = localStorage.getItem("cartNumbers");
+
+  productNumbers = parseInt(productNumbers);
+    if (productNumbers) {
+      localStorage.setItem("cartNumbers", productNumbers + 1);
+      document.querySelector(".cart span").textContent = productNumbers + 1;
+  } else {
+      localStorage.setItem("cartNumbers", 1);
+      document.querySelector(".cart span").textContent = 1;
+  }
+
+}
+onLoadCartNumbers();
