20dfa9af109f2ee7a9c0bdbaeaa70a111ee0ae58
Katrina Lyman <lymankatrina@gmail.com>
2023-03-10 00:24:47 -0700

    pulled changes from main and resolved conflicts

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-import ProductData from "./ProductData.mjs";
-import ProductListing from "./ProductList.mjs";
-
-// create instance of product data module
-const dataSource = new ProductData("tents");
-const element = document.querySelector(".product-list");
-const listing = new ProductListing("Tents", dataSource, element);
-=======
 import { loadHeaderFooter } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
@@ -16,6 +7,5 @@ loadHeaderFooter();
 const dataSource = new ProductData("tents");
 const element = document.querySelector(".product-list");
 const listing = new ProductList("Tents", dataSource, element);
->>>>>>> main
 
 listing.init();
