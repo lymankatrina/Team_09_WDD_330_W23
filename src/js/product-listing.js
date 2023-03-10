46490881217a83b6bd999a9976b36fa48e511e64
Katrina Lyman <lymankatrina@gmail.com>
2023-03-09 19:57:18 -0700

    set up correct product-listing parameters

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -1,9 +1,16 @@
 import ProductData from "./ProductData.mjs";
 import ProductListing from "./ProductList.mjs";
+import { loadHeaderFooter, getParam } from "./utils.mjs";
+
+loadHeaderFooter();
 
 // create instance of product data module
-const dataSource = new ProductData("tents");
-const element = document.querySelector(".product-list");
-const listing = new ProductListing("Tents", dataSource, element);
-
-listing.init();
+const category = getParam("category");
+// first create an instance of our ProducetData class.
+const dataSource = new ProductData();
+// get the element we want the product list to render in
+const listElement = document.querySelector(".product-list");
+// create an instance of our ProductList class and send it correct information.
+const myList = new ProductListing(category, dataSource, listElement);
+// call the init method to show our products
+MyList.init();
