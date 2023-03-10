e9506b269d884bf0db74d2d515e4d47344b18bf1
Katrina Lyman <lymankatrina@gmail.com>
2023-03-09 20:22:05 -0700

    fix shopping cart array

--- src/js/product.js
+++ src/js/product.js
@@ -1,4 +1,4 @@
-import { getParam } from "./utils.mjs";
+import { getParam, setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
@@ -8,16 +8,20 @@ const dataSource = new ProductData("tents");
 const product = new ProductDetails(productId, dataSource);
 product.init();
 
-// function addProductToCart(product) {
-//   setLocalStorage("so-cart", product);
-// }
-// // add to cart button event handler
-// async function addToCartHandler(e) {
-//   const product = await dataSource.findProductById(e.target.dataset.id);
-//   addProductToCart(product);
-// }
+function addProductToCart(product) {
+  let cart = getLocalStorage("so-cart");
+  cart = cart ?? [];
+  cart.push(product);
+  setLocalStorage("so-cart", cart);
+}
 
-// // add listener to Add to Cart button
-// document
-//   .getElementById("addToCart")
-//   .addEventListener("click", addToCartHandler);
+// add to cart button event handler
+async function addToCartHandler(e) {
+  const product = await dataSource.findProductById(e.target.dataset.id);
+  addProductToCart(product);
+}
+
+// add listener to Add to Cart button
+document
+  .getElementById("addToCart")
+  .addEventListener("click", addToCartHandler);
