61056e03d76333ab3871952b50dc71649ba884d7
Katrina Lyman <lymankatrina@gmail.com>
2023-03-10 21:32:44 -0700

    fix link to cart in header partials

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,4 +1,4 @@
-import { loadHeaderFooter } from "./utils.mjs";
+import { getLocalStorage, loadHeaderFooter } from "./utils.mjs";
 import ShoppingCart from "./ShoppingCart.mjs";
 
 loadHeaderFooter();
