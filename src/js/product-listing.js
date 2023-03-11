c7579a122b3384971e28f11e9cfba5f80960faf0
MasterP2 <arikpopeace98@gmail.com>
2023-03-11 12:58:51 -0500

    Added ProductSearchBox in the navBar

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -10,3 +10,23 @@ const element = document.querySelector(".product-list");
 const listing = new ProductList(category, dataSource, element);
 
 listing.init();
+
+
+
+
+
+// This section below adds search box to the product list page
+// Add a search box to the navbar that will allow to search for a product.
+//  Whent he form submits the search results should be retrieved from the 
+// API and displayed on a product list page.
+
+const searchForm = document.getElementById("search-form");
+const searchQuery = document.getElementById("search-query");
+
+searchForm.addEventListener("submit", event => {
+  event.preventDefault();
+  const searchValue = searchQuery.value.trim();
+  dataSource.search(searchValue).then(results => {
+    listing.update(results);
+  });
+});
