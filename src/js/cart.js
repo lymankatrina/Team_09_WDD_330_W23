ae6ca208d04f12d504f6b10c31c0806338faa18a
MasterP2 <arikpopeace98@gmail.com>
2023-03-18 11:03:17 -0400

    Fixed Checkout button

--- src/js/cart.js
+++ src/js/cart.js
@@ -3,5 +3,15 @@ import ShoppingCart from "./ShoppingCart.mjs";
 
 loadHeaderFooter();
 
+
+
+// const cart = new ShoppingCart("so-cart", ".product-list");
+// cart.renderCartContents();
+
 const cart = new ShoppingCart("so-cart", ".product-list");
-cart.renderCartContents();
+cart.init();
+if (cart.total > 0) {
+  // show our checkout button and total if there are items in the cart.
+  document.querySelector(".list-footer").classList.remove("hide");
+}
+
