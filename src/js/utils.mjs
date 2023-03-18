8145a2d770b27cdf0c2fae20075cb824bbd1ccb0
Katrina Lyman <lymankatrina@gmail.com>
2023-03-18 16:59:00 -0600

    set up alerts

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -72,3 +72,25 @@ export function setClick(selector, callback) {
   });
   qs(selector).addEventListener("click", callback);
 }
+
+export function alertMessage(message, scroll = true, duration = 3000) {
+  const alert = document.createElement("div");
+  alert.classList.add("alert");
+  alert.innerHTML = `<p>${message}</p><span>X</span>`;
+
+  alert.addEventListener("click", function (e) {
+    if (e.target.tagName == "SPAN") {
+      MediaDeviceInfo.removeChild(this);
+    }
+  });
+  const main = document.querySelector("man");
+  main.prepend(alert);
+  // make sure the alert shows by scrolling to top of window
+  // default to scroll=true, but allow it to be overridden
+  if (scroll) window.scrollTo(0, 0);
+}
+
+export function removeAllAlerts() {
+  const alerts = document.querySelectorAll(".alert");
+  alerts.forEach((alert) => document.querySelector("main").removeChild(alert));
+}
