f5ee678f1f299da62626e83ad08826eb923fcc2a
Teresa Moser <moserte@byui.edu>
2023-03-25 21:10:18 -0600

    Added the Remove Button on list

--- src/js/ShoppingCart.mjs
+++ src/js/ShoppingCart.mjs
@@ -3,6 +3,7 @@ import { getLocalStorage } from "./utils.mjs";
 function cartItemTemplate(item) {
   //  cartTotal += item.FinalPrice;
   const newItem = `<li class="cart-card divider">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <a href="#" class="cart-card__image">
     <img
       src="${item.Image}"
@@ -15,6 +16,9 @@ function cartItemTemplate(item) {
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
   <p class="cart-card__quantity">qty: 1</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
+  <p>
+  <button id="myBtn" type="button" class="btn"><i class="fa fa-trash"></i>  Remove</button>
+  </p>
 </li>`;
 
   return newItem;
@@ -31,10 +35,33 @@ export default class ShoppingCart {
     this.calculateListTotal(list);
     this.renderCartContents(list);
   }
+    // function for removing items from the cart--Teresa Moser
+  
+  removeItemFromCart(newItem) {
+    const cart = new ShoppingCart("so-cart", ".product-list");
+    for (var i in cart ) {
+      if (cart[i].newItem === newItem) {
+          cart[i].count --;
+          
+          if (cart[i].count === 0) {
+              cart.splice(i, 1); // removes item from the array
+          }
+          break;
+      }
+      removeItemFromCart()
+    }
+  
+  document
+    .getElementById("myBtn")
+    .addEventListener("click", removeItemFromCart(newItem));
+
+  }
+
   calculateListTotal(list) {
     const amounts = list.map((item) => item.FinalPrice);
     this.total = amounts.reduce((sum, item) => sum + item);
   }
+
   renderCartContents() {
     //    cartTotal = 0;
     const cartItems = getLocalStorage(this.key);
