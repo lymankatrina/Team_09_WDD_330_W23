1ea2657baed5ea7dd46edca8095e121f8148901f
Teresa Moser <moserte@byui.edu>
2024-05-07 21:11:44 -0600

    update images

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -8,7 +8,7 @@ const category = getParam("category");
 // first create an instance of our ExternalServices class.
 const dataSource = new ExternalServices();
 // then get the element we want the product list to render in
-const element = document.querySelector(".product-list");
+const element = document.querySelector("./product-list");
 // then create an instance of our ProductList class and send it the correct information.
 const listing = new ProductList(category, dataSource, element);
 // finally call the init method to show our products
